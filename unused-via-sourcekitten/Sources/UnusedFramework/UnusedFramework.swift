34bcb4a46f6e4925933b5ce79326d78553a0c7d7
Paul Taykalo <tt.kilew@gmail.com>
2017-11-09 00:22:49 +0200

    Not enough

--- unused-via-sourcekitten/Sources/UnusedFramework/UnusedFramework.swift
+++ unused-via-sourcekitten/Sources/UnusedFramework/UnusedFramework.swift
@@ -5,11 +5,25 @@ public class Unused {
     public enum ItemType: String {
         case `class`
         case `protocol`
+        case `struct`
     }
 
-    public struct Item {
+    public struct Item: Hashable {
+        public var hashValue: Int {
+            return (name.hashValue << 16) ^ type.hashValue
+        }
+
+        public static func ==(lhs: Unused.Item, rhs: Unused.Item) -> Bool {
+            return lhs.name == rhs.name && rhs.type == lhs.type
+        }
+
         public let name: String
         public let type: ItemType
+    }
+
+    enum Ref {
+        case name(String)
+        case item(Item)
     }
 
     private let source: String
@@ -31,13 +45,16 @@ public class Unused {
 
 
 private class StructureTraveser {
+
+    var usage:[Unused.Item: Int] = [:]
+
     private let root: [String: SourceKitRepresentable]
     init(root: [String: SourceKitRepresentable]) {
         self.root = root
     }
 
     func items() -> [Unused.Item] {
-        return items(from: root)
+        return items(from: root).filter { usage[$0] == nil }
     }
 
     private func items(from node: [String: SourceKitRepresentable]) -> [Unused.Item] {
@@ -49,11 +66,30 @@ private class StructureTraveser {
             items.append(.init(name: node.name(), type: .protocol))
         }
 
+        if node.kind(.struct) {
+            items.append(.init(name: node.name(), type: .struct))
+        }
+
+        node.inheritedTypes().forEach { name in
+            registerUsages(for: name)
+        }
+
+        node.typeName().map(registerUsages)
+
         let moreItems = node.substructure().flatMap({ self.items(from: $0)})
         return items + moreItems
     }
 
+    private func registerUsages(for name: String) {
+        let types : [Unused.ItemType] = [ .class, .protocol, .struct ]
+        types.forEach {
+            let item = Unused.Item(name: name, type: $0)
+            usage[item] = (usage[item] ?? 0) + 1
+        }
+    }
+
 }
+
 
 fileprivate extension Dictionary where Key == String, Value == SourceKitRepresentable {
     func substructure() -> [[String: SourceKitRepresentable]] {
@@ -64,5 +100,13 @@ fileprivate extension Dictionary where Key == String, Value == SourceKitRepresen
     }
     func name() -> String {
         return (self["key.name"] as? String) ?? ""
+    }
+
+    func typeName() -> String? {
+        return self["key.typename"] as? String
+    }
+
+    func inheritedTypes() -> [String] {
+        return (self["key.inheritedtypes"] as? [[String: SourceKitRepresentable]] ?? []).map { $0.name() }
     }
 }
--- unused-via-sourcekitten/Tests/unused-via-sourcekitten/UnusedTests.swift
+++ unused-via-sourcekitten/Tests/unused-via-sourcekitten/UnusedTests.swift
@@ -16,6 +16,15 @@ class UnusedTests: XCTestCase {
         XCTAssertTrue(unusedItem?.type == .class)
     }
 
+    func testUsedClass() {
+        let sut = generateSut(source: """
+           class Used {}
+           class Unused: Used {}
+        """)
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "Used" }))
+        XCTAssert(sut.unusedItems.contains(where: { $0.name == "Unused" }))
+    }
+
     func testUnusedProtocol() {
         let sut = generateSut(source: """
            protocol UnusedProtocol {}
@@ -25,6 +34,66 @@ class UnusedTests: XCTestCase {
         XCTAssertTrue(unusedItem?.name == "UnusedProtocol")
         XCTAssertTrue(unusedItem?.type == .protocol)
     }
+
+    func testUsedProtocol() {
+        let sut = generateSut(source: """
+           protocol Used {}
+           protocol Unused: Used {}
+        """)
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "Used" }))
+        XCTAssert(sut.unusedItems.contains(where: { $0.name == "Unused" }))
+    }
+
+    func testUnusedStruct() {
+        let sut = generateSut(source: """
+           struct UnusedStruct {}
+        """)
+        let unusedItem = sut.unusedItems.first
+        XCTAssertNotNil(unusedItem)
+        XCTAssertTrue(unusedItem?.name == "UnusedStruct")
+        XCTAssertTrue(unusedItem?.type == .struct)
+    }
+
+    func testUsedInProtocolVariable() {
+        let sut = generateSut(source: """
+           protocol UsedProtocol {}
+           struct UsedStruct {}
+           class UsedClass {}
+
+           protocol Unused {
+             var a: UsedProtocol { get }
+             var b: UsedStruct { get }
+             var c: UsedClass { get }
+            }
+        """)
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedProtocol" }))
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedStruct" }))
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedClass" }))
+
+        XCTAssert(sut.unusedItems.contains(where: { $0.name == "Unused" }))
+
+    }
+
+    func testUsedInProtocolTypeAlias() {
+        let sut = generateSut(source: """
+           protocol UsedProtocol {}
+           struct UsedStruct {}
+           class UsedClass {}
+
+           protocol Unused {
+             typealias a = UsedProtocol
+             typealias b = UsedStruct
+             typealias c = UsedClass
+            }
+        """)
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedProtocol" }))
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedStruct" }))
+        XCTAssert(!sut.unusedItems.contains(where: { $0.name == "UsedClass" }))
+
+        XCTAssert(sut.unusedItems.contains(where: { $0.name == "Unused" }))
+
+    }
+
 
     private func generateSut(source: String) -> Unused {
         return Unused(source: source)
