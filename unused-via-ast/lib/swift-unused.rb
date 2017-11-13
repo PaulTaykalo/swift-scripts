0eaea5fa0e833bdaa615dc59f9daeab34d5645dd
Paul Taykalo <tt.kilew@gmail.com>
2017-11-13 23:35:44 +0200

    Add tests on inheritance, classes, enums

--- unused-via-ast/lib/swift-unused.rb
+++ unused-via-ast/lib/swift-unused.rb
@@ -1,30 +1,49 @@
 require 'swift-ast-dump/swift_ast_parser'
 class SwiftUnused
 
-  def initialize(ast_path)
-    @ast_path = ast_path
-    @tree = SwiftAST::Parser.new().parse_build_log_output(File.read(ast_path))
-
+  def initialize(options)
+    ast_path = options[:ast_path]
+    source = options[:source]
+    @tree = SwiftAST::Parser.new().parse_build_log_output(File.read(ast_path)) if ast_path
+    @tree = SwiftAST::Parser.new().parse_build_log_output(source) if source
   end
 
   def search
     @used_classes = Hash.new
     @used_protocols = Hash.new
+    @used_structs = Hash.new
+    @used_enums = Hash.new
 
     unused_classes = []
     unused_protocols = []
+    unused_structs = []
+    unused_enums = []
     classes = @tree.find_nodes("class_decl")
     protocols = @tree.find_nodes("protocol")   
-
+    extensions = @tree.find_nodes("extension_decl")   
+    structs = @tree.find_nodes("struct_decl")
+    enums = @tree.find_nodes("enum_decl")
 
     classes.each { |classnode|
-      register_inheritance(classnode) { |inh| @used_classes[inh] = "inherited" }
+      register_inheritance(classnode) { |inh| 
+        @used_classes[inh] = "inherited" 
+        @used_protocols[inh] = "inherited" # we aren't gathering info what string is, so we'll just mark the as P and Classes
+      }
     }
 
     protocols.each { |protocolnode|
       register_inheritance(protocolnode) { |inh| @used_protocols[inh] = "inherited"}
     }
 
+    extensions.each { |extension_node|
+      register_inheritance(extension_node) { |inh| @used_protocols[inh] = "inherited"}
+    }
+
+    structs.each { |struct_node|
+      register_inheritance(struct_node) { |inh| @used_protocols[inh] = "inherited"}
+    }
+
+     
     classes.each { |node| 
       next unless classname = node.parameters.first
       next unless @used_classes[classname].nil? #// skip already used classes
@@ -37,8 +56,22 @@ class SwiftUnused
       unused_protocols += [protocol_name]
     }
 
+    structs.each { |node|
+      next unless struct_name = node.parameters.first
+      next unless @used_structs[struct_name].nil? #// skip already used structs
+      unused_structs += [struct_name]
+    }
+
+    enums.each { |node|
+      next unless enum_name = node.parameters.first
+      next unless @used_enums[enum_name].nil? #// skip already used structs
+      unused_enums += [enum_name]
+    }
+
     @classes = unused_classes
     @protocols = unused_protocols
+    @structs = unused_structs
+    @enums = unused_enums
   end  
 
   def classes
@@ -48,6 +81,14 @@ class SwiftUnused
   def protocols
     @protocols
   end 
+
+  def structs
+    @structs
+  end  
+
+  def enums
+    @enums
+  end  
 
   def register_inheritance(node, &block)
     inheritance = node.parameters.drop_while { |el| el != "inherits:" }
--- unused-via-ast/test/swift-unused_test.rb
+++ unused-via-ast/test/swift-unused_test.rb
@@ -2,36 +2,97 @@ require 'minitest/autorun'
 require 'swift-unused'
 
 class SwiftUnusedTest < Minitest::Test
-  def setup
-    @unused = create_sut
-    @unused.search
-  end  
-
-  def test_initial_state
-    assert(!@unused.nil?, "Should be able to create instance of unused")
-  end
-
   def test_unused_classes
+    with_swift_ast "0000", """
+    class A {}
+    class B {}
+    """ #, skip_cache=true
     assert_includes(@unused.classes, 'A')    
     assert_includes(@unused.classes, 'B')    
   end
 
   def test_used_classes_by_inheritance
+    with_swift_ast "0001", """
+    class C {} // used by inheritance
+    class D: C {} // unused
+    """ #, skip_cache=true
     assert_includes(@unused.classes, 'D')    
     refute_includes(@unused.classes, 'C')    
   end
 
   def test_unused_protocols
+    with_swift_ast "0002", """
+    protocol E {} // unused
+    protocol F {} // unused
+    """ #, skip_cache=true
     assert_includes(@unused.protocols, 'E')    
+    assert_includes(@unused.protocols, 'F')    
   end  
 
   def test_used_protocols_by_inheritance
+    with_swift_ast "0003", """
+    protocol F {}
+    protocol G: F {} 
+
+    protocol H {} 
+    class I: H {}
+
+    protocol J {}
+    struct K: J {}  
+    """ #, skip_cache=true
     assert_includes(@unused.protocols, 'G')    
     refute_includes(@unused.protocols, 'F')    
+    refute_includes(@unused.protocols, 'H')    
+    refute_includes(@unused.protocols, 'J')    
+
   end
 
-  def create_sut
-    SwiftUnused.new("./test/fixtures/Unused.ast")
+  def test_used_protocol_by_extension
+    with_swift_ast "0004", """
+    class C {}        // unused
+    protocol F {}     // used by extension
+    extension C: F {}
+    """ #, skip_cache=true
+    refute_includes(@unused.protocols, 'F')        
+    assert_includes(@unused.classes, 'C')    
+    
+  end
+
+  def test_unused_structs
+    with_swift_ast "0005", """
+    struct A {} // unused
+    struct B {} // unused
+    """#, skip_cache=true
+    assert_includes(@unused.structs, 'A')    
+    assert_includes(@unused.structs, 'B')    
+
+  end
+
+  def test_unused_enums
+    with_swift_ast "0006", """
+    enum A {} // unused
+    enum B {} // unused
+    """#, skip_cache=true
+    assert_includes(@unused.enums, 'A')    
+    assert_includes(@unused.enums, 'B')    
+  end
+
+
+
+  def with_swift_ast(id, source, skip_cache = false)
+    require 'tempfile'
+    generated_path =  "./test/fixtures/generated/"
+    outpath = generated_path + "#{id}.ast"
+    if !File.exist?(outpath) || skip_cache
+      inpath = generated_path + "#{id}.swift"
+      file = File.new(inpath, "w")
+      # file = Tempfile.new(['unused-test', '.swift'])
+      file.write(source)
+      file.close
+      %x(swiftc -dump-ast #{inpath} &> #{outpath} )
+    end
+    @unused = SwiftUnused.new({:ast_path => outpath})
+    @unused.search
   end  
 
 end
